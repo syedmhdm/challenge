5138c2996df133d8082c59ef2865c0594b62ab65
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-10-22 01:34:46 +0530

    useReducer challenge

--- src/App.js
+++ src/App.js
@@ -1,70 +1,137 @@
-import { useState } from "react";
+import { useReducer } from "react";
+import "./App.css";
 
-function useGeolocation() {
-  const [position, setPosition] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+/*
+INSTRUCTIONS / CONSIDERATIONS:
 
-  function getPosition() {
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
+1. Let's implement a simple bank account! It's similar to the example that I used as an analogy to explain how useReducer works, but it's simplified (we're not using account numbers here)
 
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
+2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. Use the `initialState` below to get started.
+
+3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. You can check this right at the beginning of the reducer
+
+4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500)
+
+5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
+
+6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
+
+7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
+*/
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, isActive: true, balance: action.payload.money };
+    case "deposit":
+      return { ...state, balance: state.balance + action.payload.money };
+    case "withdraw":
+      return {
+        ...state,
+        balance: state.balance <= 0 ? 0 : state.balance - action.payload.money,
+      };
+    case "requestLoan":
+      return {
+        ...state,
+        balance:
+          state.loan === 0
+            ? state.balance + action.payload.money
+            : state.balance,
+        loan: action.payload.money,
+      };
+    case "payLoan":
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+      };
+    case "closeAccount":
+      return {
+        ...state,
+        isActive: state.balance === 0 && state.loan === 0 ? false : true,
+      };
+    default:
+      return { ...state };
   }
-
-  return { position, isLoading, error, getPosition };
 }
 
 export default function App() {
-  const [countClicks, setCountClicks] = useState(0);
-
-  const {
-    position: { lat, lng },
-    isLoading,
-    error,
-    getPosition,
-  } = useGeolocation();
-
-  function handleClick() {
-    setCountClicks((count) => count + 1);
-    getPosition();
-  }
-
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   return (
-    <div>
-      <button onClick={handleClick} disabled={isLoading}>
-        Get my position
-      </button>
+    <div className='App'>
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
-      {isLoading && <p>Loading position...</p>}
-      {error && <p>{error}</p>}
-      {!isLoading && !error && lat && lng && (
-        <p>
-          Your GPS position:{" "}
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
-          >
-            {lat}, {lng}
-          </a>
-        </p>
-      )}
-
-      <p>You requested position {countClicks} times</p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "openAccount", payload: { money: 500 } });
+          }}
+          disabled={isActive}
+        >
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "deposit", payload: { money: 150 } });
+          }}
+          disabled={!isActive}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "withdraw", payload: { money: 50 } });
+          }}
+          disabled={!isActive}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "requestLoan", payload: { money: 5000 } });
+          }}
+          disabled={!isActive}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "payLoan" });
+          }}
+          disabled={!isActive}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "closeAccount" });
+          }}
+          disabled={!isActive}
+        >
+          Close account
+        </button>
+      </p>
     </div>
   );
 }
